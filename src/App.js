adcb849a094e73951186c6e3c246b48ce8b588e8
FlyingColors02 <shraduborse97@gmail.com>
2020-06-13 18:52:39 +0530

    react hook

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ const App =()=>{
     return(
       <div className="container">
         <div className="row">
-          <div class="col-md-12">
+          <div className="col-md-12">
             <button type="button" className="btn btn-outline-primary"
             onClick={()=>setData("posts")}>POSTS</button>
             <button type="button" className="btn btn-outline-primary"
-            onClick={()=>setData("albums")}>ALBUMS</button>
+            onClick={()=>setData("users")}>USERS</button>
           </div>
         </div>
         <Api value={data}/>
--- src/apicomponent.jsx
+++ src/apicomponent.jsx
@@ -1,26 +1,39 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-class Api extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            items:[]
-        }
-    }
-   async componentDidMount(){
-        let response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.value}`);
-        this.setState({items:response.data})
-    }
-    render(){
+
+
+const Api = (props) =>{
+    const [items,setItems] = useState([]);
+
+    const fetchData = async(resources)=>{
+    let response = await axios.get(`https://jsonplaceholder.typicode.com/${resources}`);
+        setItems(response.data);
+}
+
+//    async componentDidMount(){
+//         let response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.value}`);
+//         this.setState({items:response.data})
+//     }
+//     async componentDidUpdate(prevProps){
+
+//         if (prevProps.value!==this.props.value){
+//             let response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.value}`);
+//             this.setState({items:response.data})
+//         }  
+//     }
+    useEffect(()=>{
+        fetchData(props.value);
+    },[props.value]);
+    
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        {this.state.items.length}
+                        {items.length}
                     </div>
                 </div>
             </div>
         )
     }
-}
+
 export default Api;
